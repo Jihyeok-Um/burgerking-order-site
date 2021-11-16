b7c1d4d8381c9ecbb0c371a455b454b87c90d066
Jihyeok-Um <75592315+Jihyeok-Um@users.noreply.github.com>
2021-11-16 22:32:41 +0900

    bug fix

--- src/js/menuopen.js
+++ src/js/menuopen.js
@@ -1,11 +1,9 @@
-window.onload = function(){
-    const menuEmoji = document.querySelector(".menu-icon");
-    console.log("work1");
+const menuEmoji = document.querySelector(".menu-icon");
+console.log("work1");
 
-    function toggleMenuAndX(event){
-        console.log("work2");
-         menuEmoji.classList.toggle("is-active");
-    }
-
-    menuEmoji.addEventListener("click", toggleMenuAndX);
+function toggleMenuAndX(event){
+    console.log("work2");
+     menuEmoji.classList.toggle("is-active");
 }
+
+menuEmoji.addEventListener("click", toggleMenuAndX);
--- src/js/scrollScreen.js
+++ src/js/scrollScreen.js
@@ -1,23 +1,21 @@
-window.onload = function(){
-	const page = document.querySelectorAll('.section');
-	const lastPage = page.length;
-  let curPage = 1;
-  const pageHeight = window.innerHeight;
-  let curHeight = 0;
+const page = document.querySelectorAll('.section');
+const lastPage = page.length;
+let curPage = 1;
+const pageHeight = window.innerHeight;
+let curHeight = 0;
 
-  function scroll(event) {
-    if (event.deltaY > 0 && curPage < lastPage) {
-      curHeight += pageHeight;
-      window.scrollTo({top:curHeight, left:0, behavior:'smooth'})
-      curPage++;
-    }
-    else if (event.deltaY < 0 && curPage > 1) {
-      curHeight -= pageHeight;
-      window.scrollTo({top:curHeight, left:0, behavior:'smooth'})
-      curPage--;
-    }
+function scroll(event) {
+  if (event.deltaY > 0 && curPage < lastPage) {
+    curHeight += pageHeight;
+    window.scrollTo({top:curHeight, left:0, behavior:'smooth'})
+    curPage++;
   }
-  setTimeout(function()
-    {window.addEventListener("wheel", scroll);
-  },3000)
+  else if (event.deltaY < 0 && curPage > 1) {
+    curHeight -= pageHeight;
+    window.scrollTo({top:curHeight, left:0, behavior:'smooth'})
+    curPage--;
+  }
 }
+setTimeout(function()
+  {window.addEventListener("wheel", scroll);
+},3000)
