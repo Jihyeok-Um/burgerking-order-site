ac489baa6d416f7f21e9e4985173d65972b19ef5
Jihyeok-Um <75592315+Jihyeok-Um@users.noreply.github.com>
2021-11-17 08:13:01 +0900

    resizeEvent add

--- src/js/reload.js
+++ src/js/reload.js
@@ -10,3 +10,11 @@ window.onload = function () {
     },2500)
 
 }
+
+window.onresize = function () {
+    section = document.querySelectorAll(".section");
+    section.innerHeight = window.innerHeight;
+    const pageHeight = window.innerHeight;
+    window.scrollTo({top:pageHeight, left:0, behavior:'auto'});
+    console.log(section.innerHeight);
+}
