69e812a2892d20a14ece1b4ef109e87e0074bd1c
Jihyeok-Um <75592315+Jihyeok-Um@users.noreply.github.com>
2021-11-18 21:05:27 +0900

    menuButton visibility

--- src/js/menuopen.js
+++ src/js/menuopen.js
@@ -1,7 +1,9 @@
 const menuEmoji = document.querySelector(".menu-icon");
+const menu = document.querySelector(".home__menu");
 
 function toggleMenuAndX(event){
      menuEmoji.classList.toggle("is-active");
+     menu.classList.toggle("is-active");
 }
 
 menuEmoji.addEventListener("click", toggleMenuAndX);
