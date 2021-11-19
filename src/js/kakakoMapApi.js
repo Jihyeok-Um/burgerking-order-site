cbb0cfb7cb8a513c0b0e444e8620f097823c306b
Jihyeok-Um <75592315+Jihyeok-Um@users.noreply.github.com>
2021-11-19 20:45:30 +0900

    add kakaoMap api

--- src/js/kakakoMapApi.js
+++ src/js/kakakoMapApi.js
@@ -1,4 +1,4 @@
-var container = document.getElementById('map');
+var container = document.querySelector('.map');
 var options = {
     center: new kakao.maps.LatLng(33.450701, 126.570667),
     level: 3
--- src/js/menuopen.js
+++ src/js/menuopen.js
@@ -1,12 +1,12 @@
 const menuEmoji = document.querySelector(".menu-icon");
 const menu = document.querySelector(".home__menu");
 const menuList = document.querySelectorAll(".home__menu__list");
-const homeHeader = document.querySelector(".home__header");
+const header = document.querySelector("header");
 
 function toggleMenuAndX(event){
      menuEmoji.classList.toggle("is-active");
      menu.classList.toggle("is-active");
-     homeHeader.classList.toggle("is-active");
+     header.classList.toggle("is-active");
      for (let i=0; i < menuList.length; i++) {
           menuList[i].classList.toggle("is-active");
      }
