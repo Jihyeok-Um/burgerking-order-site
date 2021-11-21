f70feb36c474541739e6d3925346b676c3491607
Jihyeok-Um <75592315+Jihyeok-Um@users.noreply.github.com>
2021-11-21 13:30:54 +0900

    scrollScreen refatoring

--- src/js/menuTreeOpen.js
+++ src/js/menuTreeOpen.js
@@ -1,8 +1,8 @@
-const menuTree = document.querySelectorAll(".menu-tree-class-for-toggle");
+const wholeenuTree = document.querySelectorAll(".menu-tree-class-for-toggle");
 
 $(".menu-icon").addEventListener("click", () => {
      for (let i=0; i < menuTree.length; i++) {
-          menuTree[i].classList.toggle("is-active");
+          wholeenuTree[i].classList.toggle("is-active");
      }
      $("header").classList.toggle("is-active");
 });
--- src/js/scrollScreen.js
+++ src/js/scrollScreen.js
@@ -1,24 +1,21 @@
-const page = document.querySelectorAll('.section');
-const lastPage = page.length;
-let curPage = 0;
-let curHeight = 0;
+const lastPage = document.querySelectorAll('.section').length;
+let curPage = curHeight = 0;
 
-function scroll(event) {
-  if(menuTree.classList.contains("is-active")){
-    return 0;
-  }
-
-  const pageHeight = window.innerHeight;
-  if (event.deltaY > 0 && curPage < lastPage){ 
-    curPage++;
-    window.scrollTo({top:curPage*pageHeight, left:0, behavior:'smooth'})
-  }
-  else if (event.deltaY < 0 && curPage > 1) {
-    curPage--;
-    window.scrollTo({top:curPage*pageHeight, left:0, behavior:'smooth'})
-  }
-}
-setTimeout(function()
-  {window.addEventListener("wheel", scroll);
+setTimeout(() => {
+  window.addEventListener("wheel", (e) => 
+  {
+    if($(".menu-tree").classList.contains("is-active")){
+      return;
+    }
+    const pageHeight = window.innerHeight;
+    if (e.deltaY > 0 && curPage < lastPage){ 
+      curPage++;
+      window.scrollTo({top:curPage*pageHeight, left:0, behavior:'smooth'})
+    }
+    else if (e.deltaY < 0 && curPage > 1) {
+      curPage--;
+      window.scrollTo({top:curPage*pageHeight, left:0, behavior:'smooth'})
+    }
+  });
 },2800)
 
