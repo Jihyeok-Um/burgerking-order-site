2beb639f51b9dd4cb8fca3c5c6f253c85078e2ab
Jihyeok-Um <75592315+Jihyeok-Um@users.noreply.github.com>
2021-11-21 13:10:24 +0900

    menuTreeOpen refactoring

--- src/js/menuTreeOpen.js
+++ src/js/menuTreeOpen.js
@@ -1,15 +1,8 @@
-const menuEmoji = document.querySelector(".menu-icon");
-const menuTree = document.querySelector(".menu-tree");
-const menuTreeList = document.querySelectorAll(".menu-tree-list");
-const header = document.querySelector("header");
+const menuTree = document.querySelectorAll(".menu-tree-class-for-toggle");
 
-function toggleMenuAndX(event){
-     menuEmoji.classList.toggle("is-active");
-     menuTree.classList.toggle("is-active");
-     header.classList.toggle("is-active");
-     for (let i=0; i < menuTreeList.length; i++) {
-          menuTreeList[i].classList.toggle("is-active");
+$(".menu-icon").addEventListener("click", () => {
+     for (let i=0; i < menuTree.length; i++) {
+          menuTree[i].classList.toggle("is-active");
      }
-}
-
-menuEmoji.addEventListener("click", toggleMenuAndX);
+     $("header").classList.toggle("is-active");
+});
